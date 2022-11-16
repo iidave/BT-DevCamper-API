29cfa322b36df53702c509781601aab7afc29880
David Winfield <iidave@msn.com>
2022-11-16 07:57:48 -0800

    User login, tokens, and cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc     Register User
-// @route    GET /api/v1/auth/register
+// @route    POST /api/v1/auth/register
 // @access   Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,7 +17,58 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({
-    success: true,
-  });
+  sendTokenResponse(user, 200, res);
 });
+
+// @desc     Login User
+// @route    POST /api/v1/auth/login
+// @access   Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Validate email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse('Please provide a valid email and password', 400)
+    );
+  }
+
+  //Check for User
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if(process.env.NODE_ENV === 'production') {
+    options.secure = true;
+
+  }
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
